a541f7cb41e807a007460759720e28a10520d2d7
Pathel7 <mpoleshapaulin70@gmail.com>
2024-06-13 11:59:32 +0100

    refactor: revision des useCases du model Intervenants

--- lib/m_evaluation/business/interactor/intervenants/GetIntervenantLocalUseCase.dart
+++ lib/m_evaluation/business/interactor/intervenants/GetIntervenantLocalUseCase.dart
@@ -1,12 +1,10 @@
 import 'package:odc_mobile_project/m_evaluation/business/model/Vote/intervenants.dart';
 import '../../services/evaluationLocalService.dart';
-import '../../services/evaluationNetworkService.dart';
 
 class GetIntervenantLocalUseCase{
-  EvaluationNetworkService network;
   EvaluationLocalService local;
 
-  GetIntervenantLocalUseCase(this.network, this.local);
+  GetIntervenantLocalUseCase( this.local);
   Future<Intervenants> run() async{
    var res = await local.getIntervenant();
    return res;
