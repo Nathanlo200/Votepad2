f23de2b82ca1e533621dbef92a985c918861a5c3
donatiennemanyong <donatiennemanyong7@gmail.com>
2024-06-17 14:04:58 +0100

    feat: declaration du useCase saveIntervenantLocalUseCase dans EvaluationInteractor.dart

--- lib/m_evaluation/business/interactor/EvaluationInteractor.dart
+++ lib/m_evaluation/business/interactor/EvaluationInteractor.dart
@@ -1,4 +1,6 @@
+import 'package:odc_mobile_project/m_evaluation/business/interactor/jurys/GetJuryNetworkUseCase.dart';
 import 'package:odc_mobile_project/m_evaluation/business/interactor/phases/GetPhaseByIntervenantNetworkUseCase.dart';
+import 'package:odc_mobile_project/m_evaluation/business/interactor/phases/GetPhasesListNetworkUseCase.dart';
 import 'package:odc_mobile_project/m_evaluation/business/interactor/questions/GetQuestionListByPhaseNetworkUseCase.dart';
 import 'package:odc_mobile_project/m_evaluation/business/interactor/reponses/GetReponseListLocalUseCase.dart';
 import 'package:odc_mobile_project/m_evaluation/business/interactor/reponses/PostReponseNetworkUseCase.dart';
@@ -17,38 +19,23 @@ part 'EvaluationInteractor.g.dart';
 
 class EvaluationInteractor{
   GetIntervenantNetworkUseCase getIntervenantNetworkUseCase;
-  GetIntervenantLocalUseCase getIntervenantLocalUseCase;
-  GetAssertionListNetworkUsecase getAssertionListUsecase;
-  GetQuestionListByPhaseNetworkUseCase getQuestionListByPhaseUseCase;
-  GetPhaseByIntervenantNetworkUseCase getPhaseByIntervenantuseCase;
-  PostReponseNetworkUseCase postReponseUseCase;
-  SaveReponseLocalUseCase saveReponseUseCase;
-  SaveIntervenantLocalUseCase saveIntervenantUseCase;
-  GetReponseListLocalUseCase getReponseListUseCase;
-  
+  GetJuryNetworkUseCase getJuryNetworkUseCase;
+  GetPhasesListNetworkUseCase getPhasesListNetworkUseCase;
+  SaveIntervenantLocalUseCase saveIntervenantLocalUseCase;
+
   EvaluationInteractor._(
       this.getIntervenantNetworkUseCase,
-      this.getAssertionListUsecase,
-      this.getPhaseByIntervenantuseCase,
-      this.getQuestionListByPhaseUseCase,
-      this.postReponseUseCase,
-      this.saveReponseUseCase,
-      this.getIntervenantLocalUseCase,
-      this.saveIntervenantUseCase,
-      this.getReponseListUseCase
+      this.getJuryNetworkUseCase,
+      this.getPhasesListNetworkUseCase,
+      this.saveIntervenantLocalUseCase
       );
 
   static build(EvaluationNetworkService network, EvaluationLocalService local ){
     return EvaluationInteractor._(
         GetIntervenantNetworkUseCase(network),
-      GetAssertionListNetworkUsecase(network,),
-      GetPhaseByIntervenantNetworkUseCase(network, local),
-      GetQuestionListByPhaseNetworkUseCase(network),
-      PostReponseNetworkUseCase(network, local),
-      SaveReponseLocalUseCase(local),
-      GetIntervenantLocalUseCase( local),
-      SaveIntervenantLocalUseCase(local),
-      GetReponseListLocalUseCase(local)
+      GetJuryNetworkUseCase(network,local),
+      GetPhasesListNetworkUseCase(network,local),
+      SaveIntervenantLocalUseCase(local)
     );
   }
 }
