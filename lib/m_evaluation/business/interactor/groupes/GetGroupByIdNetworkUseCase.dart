69ba119fb1da4323eff365238c9000c26425523a
Pathel7 <mpoleshapaulin70@gmail.com>
2024-07-08 12:39:11 +0100

    feat: Ajout des usecases pour la récuperation d'un ou plusieurs groupes

--- lib/m_evaluation/business/interactor/groupes/GetGroupByIdNetworkUseCase.dart
+++ lib/m_evaluation/business/interactor/groupes/GetGroupByIdNetworkUseCase.dart
@@ -1,14 +1,14 @@
-import '../../model/Vote/phaseIntervenant.dart';
-import '../../services/evaluationNetworkService.dart';
-
-class GetGroupByIdNetworkUseCase{
-
-  EvaluationNetworkService network;
-
-  GetGroupByIdNetworkUseCase(this.network);
-
-  Future<PhaseIntervenant> run(int id) async {
-    var res = await network.getGroupById(id);
-    return res;
-  }
-}
+// import '../../model/Vote/phaseIntervenant.dart';
+// import '../../services/evaluationNetworkService.dart';
+//
+// class GetGroupByIdNetworkUseCase{
+//
+//   EvaluationNetworkService network;
+//
+//   GetGroupByIdNetworkUseCase(this.network);
+//
+//   Future<PhaseIntervenant> run(int id) async {
+//     var res = await network.getGroupById(id);
+//     return res;
+//   }
+// }
