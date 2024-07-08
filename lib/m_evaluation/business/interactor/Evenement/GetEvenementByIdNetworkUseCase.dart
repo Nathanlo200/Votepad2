1510e9fc771a181e31920c4171446ef8c0683296
Pathel7 <mpoleshapaulin70@gmail.com>
2024-07-08 12:40:16 +0100

    feat: Ajout des usecases pour la récuperation et le sauvegarde d'un évenèment

--- lib/m_evaluation/business/interactor/Evenement/GetEvenementByIdNetworkUseCase.dart
+++ lib/m_evaluation/business/interactor/Evenement/GetEvenementByIdNetworkUseCase.dart
@@ -1,13 +1,13 @@
-import 'package:odc_mobile_project/m_evaluation/business/model/Evenement.dart';
-import '../../services/evaluationNetworkService.dart';
-
-class GetEvenementByIdNetworkUseCase{
-  EvaluationNetworkService network;
-
-  GetEvenementByIdNetworkUseCase(this.network);
-
-  Future<Evenement> run(int questionId) async {
-    var res = await network.getEvenementById(questionId);
-    return res;
-  }
-}
+// import 'package:odc_mobile_project/m_evaluation/business/model/Evenement.dart';
+// import '../../services/evaluationNetworkService.dart';
+//
+// class GetEvenementByIdNetworkUseCase{
+//   EvaluationNetworkService network;
+//
+//   GetEvenementByIdNetworkUseCase(this.network);
+//
+//   Future<Evenement> run(int questionId) async {
+//     var res = await network.getEvenementById(questionId);
+//     return res;
+//   }
+// }
