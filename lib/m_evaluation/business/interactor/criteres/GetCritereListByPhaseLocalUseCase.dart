391a983469c29b4ccee5ed0fda5d7a9ddce63f3f
Nathanlo200 <nathanlomito@gmail.com>
2024-07-18 16:28:39 +0100

    suppression du paramettre du type network dans le useCase du service local

--- lib/m_evaluation/business/interactor/criteres/GetCritereListByPhaseLocalUseCase.dart
+++ lib/m_evaluation/business/interactor/criteres/GetCritereListByPhaseLocalUseCase.dart
@@ -1,15 +1,12 @@
-// import '../../model/Vote/phaseCriteres.dart';
-// import '../../services/evaluationLocalService.dart';
-// import '../../services/evaluationNetworkService.dart';
-//
-// class GetCritereListByPhaseLocalUseCase {
-//   EvaluationNetworkService network;
-//   EvaluationLocalService local;
-//
-//   GetCritereListByPhaseLocalUseCase(this.network, this.local);
-//
-//   Future<List<PhaseCriteres>?> run() async {
-//     var res = await local.getCritereListByPhase();
-//     return res;
-//   }
-// }
+import '../../model/Vote/phaseCriteres.dart';
+import '../../services/evaluationLocalService.dart';
+class GetCritereListByPhaseLocalUseCase {
+  EvaluationLocalService local;
+
+  GetCritereListByPhaseLocalUseCase( this.local);
+
+  Future<List<PhaseCriteres>?> run() async {
+    var res = await local.getCritereListByPhase();
+    return res;
+  }
+}
