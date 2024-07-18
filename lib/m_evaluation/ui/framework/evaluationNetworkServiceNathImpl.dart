0a290378be5de2c14a44118c0dd5a6a1fb1f5c99
Nathanlo200 <nathanlomito@gmail.com>
2024-07-18 16:39:48 +0100

    ajout du fichier test pour l'implementation du network et local service

---
+++ lib/m_evaluation/ui/framework/evaluationNetworkServiceNathImpl.dart
@@ -0,0 +1,163 @@
+import 'dart:convert';
+import 'package:odc_mobile_project/m_evaluation/business/model/Evenement.dart';
+import 'package:odc_mobile_project/m_evaluation/business/model/Vote/PhasesVote.dart';
+import 'package:odc_mobile_project/m_evaluation/business/model/Vote/createVoteRequest.dart';
+import 'package:odc_mobile_project/m_evaluation/business/model/Vote/groupes.dart';
+import 'package:odc_mobile_project/m_evaluation/business/model/Vote/intervenants.dart';
+import 'package:odc_mobile_project/m_evaluation/business/model/Vote/jurys.dart';
+import 'package:odc_mobile_project/m_evaluation/business/model/Vote/phaseCriteres.dart';
+import 'package:odc_mobile_project/m_evaluation/business/model/intervenant/phaseIntervenant.dart';
+import "package:http/http.dart" as http;
+
+import '../../business/model/evaluation/assertions.dart';
+import '../../business/model/evaluation/postReponses.dart';
+import '../../business/model/evaluation/questionAssertions.dart';
+import '../../business/services/evaluationNetworkService.dart';
+
+class EvaluationNetworkServiceNathImpl implements EvaluationNetworkService{
+  String baseURL;
+
+  EvaluationNetworkServiceNathImpl(this.baseURL);
+
+  @override
+  Future<List<Assertions>> getAssertionList(int questionId) async{
+    var res= await http.get(Uri.parse("$baseURL/api/assertions"),
+        headers: {});
+    var reponseList=json.decode(res.body) as List;
+    //print("responseMap $reponseList");
+    await Future.delayed(Duration(seconds: 3));
+    var responseFinal= reponseList.map((e)=> Assertions.fromJson(e) ).toList();
+    return responseFinal;
+  }
+
+
+  @override
+  Future<Intervenants?> getIntervenant(String email, String coupon) async {
+    var res = await http.post(
+        Uri.parse("$baseURL/api/intervenants-authenticate"),
+        body: {"email": email, "coupon": coupon});
+    // print("body response ${res.body}");
+    // print(res.statusCode);
+    if ([200, 201].indexOf(res.statusCode) == -1) {
+      throw Exception(res.body);
+    }
+    var reponseMap = json.decode(res.body) as Map;
+    print("responseMap $reponseMap");
+    var reponseFinal = Intervenants.fromJson(reponseMap.cast<String, dynamic>());
+    return reponseFinal;
+  }
+
+  @override
+  Future<PhaseIntervenant> getPhasesByIntervenant(int intervenantId, int evenementId) async{
+    var res= await http.get(Uri.parse("$baseURL/api/phases"),
+        headers: {"Authorization": "Bearer $evenementId"});
+    var reponseBody = json.decode(res.body) as PhaseIntervenant;
+    await Future.delayed(Duration(seconds: 3));
+    var responseFinal = PhaseIntervenant.fromJson(reponseBody as Map);
+    return responseFinal;
+
+  }
+
+  @override
+  Future<List<QuestionsAssertions>> getQuestionListByPhase(int phaseId) async {
+    var res= await http.get(Uri.parse("$baseURL/api/questions"));
+    var reponseList=json.decode(res.body) as List;
+    //print("responseMap $reponseList");
+    await Future.delayed(Duration(seconds: 1));
+    var responseFinal= reponseList.map((e)=> QuestionsAssertions.fromJson(e) ).toList();
+    return responseFinal;
+  }
+
+  @override
+  Future<bool> postReponses(List<PostReponses> data) async{
+    await http.post(Uri.parse("$baseURL/api/reponses"));
+    return Future.value(true);
+  }
+
+  @override
+  Future<Evenement> getEvenementById(int id) async {
+    var res= await http.get(Uri.parse("$baseURL/api/phases"),);
+    print(res.body);
+    var reponseList=json.decode(res.body) as List;
+    print("responseMap $reponseList");
+    var responseFinal= reponseList.map((e)=> Evenement.fromJson(e) ).toList();
+    return responseFinal as Evenement;
+  }
+
+  @override
+  Future<PhasesVote> getPhaseListById(int id) {
+    // TODO: implement getPhaseListById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<PhasesVote>> getPhasesList()  async{
+    var res= await http.get(Uri.parse("$baseURL/api/phases"),);
+    print(res.body);
+    var reponseList=json.decode(res.body) as List;
+    print("responseMap $reponseList");
+    var responseFinal= reponseList.map((e)=> PhasesVote.fromJson(e) ).toList();
+    return responseFinal;
+  }
+  @override
+  Future<List<PhaseCriteres>?> getCritereListByPhase(int phaseId) async {
+    var res= await http.get(Uri.parse("$baseURL/api/phase-criteres/"),);
+    var reponseList=json.decode(res.body) as List;
+    print("responseMap $reponseList");
+    var responseFinal= reponseList.map((e)=> PhaseCriteres.fromJson(e) ).toList();
+    return responseFinal;
+  }
+
+  @override
+  Future<PhaseIntervenant> getGroupById(int id) {
+    // TODO: implement getGroupById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Groupes>?> getGroupeList(int phaseId) {
+    // TODO: implement getGroupeList
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<PhaseIntervenant> getIntervenantById(int id) {
+    // TODO: implement getIntervenantById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Intervenants>?> getIntervenantList(int phaseId) async {
+    var res= await http.get(Uri.parse("$baseURL/api/intervenant-phases/$phaseId"),);
+    var reponseList=json.decode(res.body) as List;
+    print("responseList = $reponseList");
+    var responseFinal= reponseList.map((e)=> Intervenants.fromJson(e) ).toList();
+    return responseFinal;
+  }
+
+
+  @override
+  Future<Jury?> getJury(String coupon) {
+    // TODO: implement getJury
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<CreateVoteRequest?> getVoteByGroupe(int groupeId) {
+    // TODO: implement getVoteByGroupe
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<CreateVoteRequest?> getVoteByIntervenant(int intervenantId) {
+    // TODO: implement getVoteByIntervenant
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> sendVoteByCandidat(CreateVoteRequest data) {
+    // TODO: implement sendVoteByCandidat
+    throw UnimplementedError();
+  }
+
+}
