6b0594d2b0f5c850325484d39c0b41c3b45f3189
donatiennemanyong <donatiennemanyong7@gmail.com>
2024-07-19 16:15:04 +0100

    refactor: ajout de la page de saisie de coupon pour les jurys

---
+++ lib/m_evaluation/ui/pages/SaisieCouponPage/SaisieCouponCtrl.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'SaisieCouponCtrl.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$saisieCouponCtrlHash() => r'55664ac7b5c4a862802edb5efc7648ee29cb8e76';
+
+/// See also [SaisieCouponCtrl].
+@ProviderFor(SaisieCouponCtrl)
+final saisieCouponCtrlProvider =
+    AutoDisposeNotifierProvider<SaisieCouponCtrl, SaisieCouponState>.internal(
+  SaisieCouponCtrl.new,
+  name: r'saisieCouponCtrlProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$saisieCouponCtrlHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SaisieCouponCtrl = AutoDisposeNotifier<SaisieCouponState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
