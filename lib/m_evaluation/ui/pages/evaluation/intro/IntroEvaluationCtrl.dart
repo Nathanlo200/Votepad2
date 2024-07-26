72816091098dd3de22399e48965d2489683f6e37
Pathel7 <mpoleshapaulin70@gmail.com>
2024-07-26 14:52:29 +0100

    feat : Ajout du state et du controller pour IntroEvaluationPage

---
+++ lib/m_evaluation/ui/pages/evaluation/intro/IntroEvaluationCtrl.dart
@@ -0,0 +1,23 @@
+import 'package:odc_mobile_project/m_evaluation/business/interactor/EvaluationInteractor.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import 'IntroEvaluationState.dart';
+
+part 'IntroEvaluationCtrl.g.dart';
+
+@riverpod
+class IntroEvaluationCtrl extends _$IntroEvaluationCtrl {
+  @override
+  IntroEvaluationState build() {
+    return IntroEvaluationState();
+  }
+
+  void getPhasename() async{
+    var useCase = ref.watch(evaluationInteractorProvider).getIntervenantLocalUseCase;
+    var phaseName = await useCase.run();
+    state = state.copyWith(
+      phaseNom: phaseName.phaseName
+    );
+  }
+
+}
