188d513e1b2fb72fc6710464960f67ff8fd573ae
Nathanlo200 <nathanlomito@gmail.com>
2024-08-06 11:02:11 +0100

    ajoute d'une  nouvelle variable critere du types model Criteres pour le model phaseCriteres

--- lib/m_evaluation/business/model/Vote/phaseCriteres.dart
+++ lib/m_evaluation/business/model/Vote/phaseCriteres.dart
@@ -1,18 +1,21 @@
 import 'dart:convert';
 
+import 'package:odc_mobile_project/m_evaluation/business/model/Vote/critere.dart';
+
+
 class PhaseCriteres {
     int id;
     int phaseId;
-    int critereId;
+    Criteres critere;
     int echelle;
     DateTime createdAt;
     DateTime updatedAt;
 
     PhaseCriteres({
         required this.id,
-        this.phaseId = 1,
-        this.critereId = 1,
-        this.echelle = 1,
+        this.phaseId =0,
+        required this.critere,
+        this.echelle= 0,
         required this.createdAt,
         required this.updatedAt,
     });
@@ -21,19 +24,19 @@ class PhaseCriteres {
 
     String toRawJson() => json.encode(toJson());
 
-    factory PhaseCriteres.fromJson(Map<String, dynamic> json) => PhaseCriteres(
-        id: json["id"] ?? 1,
-        phaseId: json["phase_id"] ?? 0,
-        critereId: json["critere_id"] ?? 0,
-        echelle: json["echelle"] ?? 0,
+    factory PhaseCriteres.fromJson(Map json) => PhaseCriteres(
+        id: json["id"]?? 0,
+        phaseId: json["phase_id"]?? 0,
+        critere: json["critere"] == null ?  Criteres.fromJson({})  : Criteres.fromJson(json["critere"]),
+        echelle: json["echelle"]?? 0,
         createdAt: json["created_at"]!=null? DateTime.parse(json["created_at"] ?? ""): DateTime.now(),
-        updatedAt: json["updated_at"]!=null? DateTime.parse(json["updated_at"] ?? "") : DateTime.now(),
+        updatedAt: json["updated_at"]!=null? DateTime.parse(json["updated_at"] ?? ""): DateTime.now(),
     );
 
     Map<String, dynamic> toJson() => {
         "id": id,
         "phase_id": phaseId,
-        "critere_id": critereId,
+        "critere": critere.toJson(),
         "echelle": echelle,
         "created_at": createdAt.toIso8601String(),
         "updated_at": updatedAt.toIso8601String(),
