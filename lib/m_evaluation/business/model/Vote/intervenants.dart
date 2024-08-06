7c78fe9ca71657e453711633b6da8fc7565a868c
Pathel7 <mpoleshapaulin70@gmail.com>
2024-08-06 14:54:57 +0100

    change : changement des champs phaseIntervenantId en phaseId, name en phaseName et ajout du champs intervenantI dans le model Intervenant

--- lib/m_evaluation/business/model/Vote/intervenants.dart
+++ lib/m_evaluation/business/model/Vote/intervenants.dart
@@ -2,8 +2,9 @@ import 'dart:convert';
 
 class Intervenants {
     int id;
-    int phasesIntervenantsId;
-    String name;
+    int phaseId;
+    int intervenantId;
+    String phaseName;
     String email;
     DateTime createdAt;
     DateTime updatedAt;
@@ -11,8 +12,9 @@ class Intervenants {
 
     Intervenants({
         required this.id,
-        this.phasesIntervenantsId = 1,
-        this.name = "",
+        this.phaseId = 1,
+        this.intervenantId = 1,
+        this.phaseName = "",
         this.email ="",
         required this.createdAt,
         required this.updatedAt,
@@ -25,8 +27,9 @@ class Intervenants {
 
     factory Intervenants.fromJson(Map<String, dynamic> json) => Intervenants(
         id: json["id"] ?? 1,
-        phasesIntervenantsId: json["phases_intervenants_id"] ?? 1,
-        name: json["name"] ?? "",
+        phaseId: json["phase_id"] ?? 1,
+        intervenantId: json["intervenant"] ?? 1,
+        phaseName: json["phase_nom"] ?? "",
         email: json["email"] ?? "",
         isDone: json["isDone"] ?? false,
         createdAt: json["created_at"]!=null? DateTime.parse(json["created_at"] ?? ""): DateTime.now(),
@@ -35,8 +38,9 @@ class Intervenants {
 
     Map<String, dynamic> toJson() => {
         "id": id,
-        "phases_intervenants_id": phasesIntervenantsId,
-        "name": name,
+        "phase_id": phaseId,
+        "intervenant": intervenantId,
+        "phase_nom": phaseName,
         "email": email,
         "isDone": isDone,
         "created_at": createdAt.toIso8601String(),
