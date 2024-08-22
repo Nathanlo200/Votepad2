ed984511da3834fe2e24bbb3a648cee275c06bc1
Pathel7 <mpoleshapaulin70@gmail.com>
2024-08-22 13:48:30 +0100

    feat : Ajout des variables eventNom, duree et mytime dans IntroEvaluationState

--- lib/m_evaluation/ui/pages/evaluation/intro/IntroEvaluationState.dart
+++ lib/m_evaluation/ui/pages/evaluation/intro/IntroEvaluationState.dart
@@ -1,15 +1,26 @@
 
 class IntroEvaluationState {
   String phaseNom;
-
+  String eventNom;
+  int? duree;
+  List<int> mytime = [];
   IntroEvaluationState({
-    this.phaseNom = ""
+    this.phaseNom = "",
+    this.eventNom = "",
+    this.duree = 0,
+    this.mytime =const []
   });
 
   IntroEvaluationState copyWith({
-    String ? phaseNom
+    String ? phaseNom,
+    String ? eventNom,
+    int ? duree,
+    List<int> ? mytime,
   }) =>
       IntroEvaluationState(
-          phaseNom: phaseNom ?? this.phaseNom
+          phaseNom: phaseNom ?? this.phaseNom,
+          eventNom: eventNom ?? this.eventNom,
+          duree: duree ?? this.duree,
+          mytime: mytime ?? this.mytime
       );
 }
