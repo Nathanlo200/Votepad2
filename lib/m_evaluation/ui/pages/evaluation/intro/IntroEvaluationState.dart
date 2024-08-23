8c097a714e1e9e1dbe53a012c8236e2b8ad6f71d
Pathel7 <mpoleshapaulin70@gmail.com>
2024-08-23 09:36:07 +0100

    feat : Ajout des variables hasBody et questions dans le fichier IntroEvaluationState.dart

--- lib/m_evaluation/ui/pages/evaluation/intro/IntroEvaluationState.dart
+++ lib/m_evaluation/ui/pages/evaluation/intro/IntroEvaluationState.dart
@@ -1,26 +1,36 @@
 
+import '../../../../business/model/evaluation/questionAssertions.dart';
+
 class IntroEvaluationState {
   String phaseNom;
   String eventNom;
   int? duree;
   List<int> mytime = [];
+  bool hasBody;
+  List<QuestionsAssertions>? questions;
   IntroEvaluationState({
     this.phaseNom = "",
     this.eventNom = "",
-    this.duree = 0,
-    this.mytime =const []
+    this.duree = null,
+    this.mytime =const [],
+    this.hasBody = true,
+    this.questions = const []
   });
 
   IntroEvaluationState copyWith({
     String ? phaseNom,
+    bool ? hasBody,
     String ? eventNom,
     int ? duree,
     List<int> ? mytime,
+    List<QuestionsAssertions>? questions
   }) =>
       IntroEvaluationState(
           phaseNom: phaseNom ?? this.phaseNom,
           eventNom: eventNom ?? this.eventNom,
           duree: duree ?? this.duree,
-          mytime: mytime ?? this.mytime
+          hasBody: hasBody ?? this.hasBody,
+          mytime: mytime ?? this.mytime,
+          questions: questions ?? questions
       );
 }
