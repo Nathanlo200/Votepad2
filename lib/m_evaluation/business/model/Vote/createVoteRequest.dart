c7433edc25ed846c72e7e49065e2e595e7c3ad13
Nathanlo200 <nathanlomito@gmail.com>
2024-08-27 13:03:25 +0100

    mis a jour du model CreateVoteRequest ajout du champ nombre_user pour envois des votes

--- lib/m_evaluation/business/model/Vote/createVoteRequest.dart
+++ lib/m_evaluation/business/model/Vote/createVoteRequest.dart
@@ -6,11 +6,13 @@ class CreateVoteRequest {
   int? intervenantId;
   int? phaseId;
   List<ResultatVotes>? cote;
+  int nombre_user;
 
   CreateVoteRequest({
     this.intervenantId,
     this.phaseId,
     this.cote,
+    this.nombre_user = 0
   });
 
   factory CreateVoteRequest.fromRawJson(String str) => CreateVoteRequest.fromJson(json.decode(str));
@@ -21,11 +23,13 @@ class CreateVoteRequest {
     intervenantId: json["intervenant_id"],
     phaseId: json["phase_id"],
     cote: json["cote"] == null ? [] : List<ResultatVotes>.from(json["cote"]!.map((x) => ResultatVotes.fromJson(x))),
+    nombre_user: json["nombre_user"]??0
   );
 
   Map<String, dynamic> toJson() => {
     "intervenant_id": intervenantId,
     "phase_id": phaseId,
-    "cote": cote == null ? [] : cote!.map((x) => x.toJson()),
+    "cote": cote == null ? [] : cote!.map((x) => x.toJson()).toList(),
+    "nombre_user": nombre_user
   };
 }
